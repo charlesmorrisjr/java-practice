b6849a7c0b280d4d3426a08a76466c98c6b2ac35
Charles Morris Jr <cmorrisjr777@gmail.com>
2026-02-18 12:35:47 -0500

    Polymorphism practice with super

--- Animal.java
+++ Animal.java
@@ -2,15 +2,13 @@
 
 class Animal {
     public void animalSound() {
-        System.out.println("The animal makes a sound.");
+        System.out.println("The animal makes a sound:");
     }
 
     public static void main(String[] args) {
-        Animal animal = new Animal();
         Pig pig = new Pig();
         Dog dog = new Dog();
 
-        animal.animalSound();
         pig.animalSound();
         dog.animalSound();
     }
@@ -18,12 +16,14 @@ class Animal {
 
 class Pig extends Animal {
     public void animalSound() {
+        super.animalSound();
         System.out.println("The pig says 'Oink!'");
     }
 }
 
 class Dog extends Animal {
     public void animalSound() {
+        super.animalSound();
         System.out.println("The dog says 'Bow-wow!'");
     }
 }
